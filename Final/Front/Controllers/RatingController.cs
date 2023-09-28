869cdaab575d70a73eab5fd5c2f07b4b1a466a0a
joannekao99 <007joannekao@gmail.com>
2023-09-28 17:13:08 +0800

    Rating合併

---
+++ Final/Front/Controllers/RatingController.cs
@@ -0,0 +1,24 @@
+﻿using Project2.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2.Controllers
+{
+    public class RatingController : Controller
+    {
+		// GET: Rating
+		public ActionResult Index()
+		{
+			// 使用 RatingService 或 EF DbContext 從數據庫中獲取評論數據
+			var ratingService = new RatingService();
+			int mediaInfoId = 26;
+
+			// 將評論數據傳遞給視圖
+			var ratings = ratingService.GetRatings(mediaInfoId);
+			return View(ratings);
+		}
+	}
+}
---
+++ Final/Front/Models/Services/RatingService.cs
@@ -0,0 +1,59 @@
+﻿using Project2.Models.DTOs;
+using Project2.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2.Models.Services
+{
+	public class RatingService
+	{//連線AppDbContext
+		private AppDbContext db = new AppDbContext();
+
+		//新增評分
+		public void Create(Rating rating)
+		{
+			db.Ratings.Add(rating);
+			db.SaveChanges();
+		}
+
+		//獲取評分列表
+		public List<RatingDTO> GetRatings(int mediaInfoId)
+		{
+			var ratings = db.Ratings
+				.Where(r => r.MediaInfoId == mediaInfoId)
+				.OrderByDescending(r => r.CreatedTime)
+				.Select(r => new RatingDTO
+				{
+					Id = r.Id,
+					MemberAccount = r.Member.Account,
+					MovieTitle = r.MediaInfo.Title,
+					Comment = r.Comment,
+					Rate = r.Rate ?? 0, // 使用 null 合併運算子設置默認值
+					CreatedTime = r.CreatedTime ?? DateTime.MinValue // 使用 null 合併運算子設置默認日期/時間
+				})
+				.ToList();
+
+			return ratings;
+		}
+
+
+		//計算平均評分
+		public double CalculateAverageRating(int mediaInfoId)
+		{
+			var ratings = db.Ratings.Where(r => r.MediaInfoId == mediaInfoId).ToList();
+			if (ratings.Count == 0)
+			{
+				return 0.0;
+			}
+
+			var totalRate = ratings.Sum(r => r.Rate);
+			var averageRating = (double)totalRate / ratings.Count;
+
+			// 平均分數只保留一位小數
+			return Math.Round(averageRating, 1);
+
+		}
+	}
+}
